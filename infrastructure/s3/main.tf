7c1a0d35f0a5dba1ac78cf94889e638b76757d2e
Matt Perreault <mattperreault@pop-os.localdomain>
2023-02-23 22:09:47 -0700

    CP-3: Create terraform stack for S3 website

--- infrastructure/s3/main.tf
+++ infrastructure/s3/main.tf
@@ -7,6 +7,66 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "cloud-profile-website"
+module "template_files" {
+  source   = "hashicorp/dir/template"
+  version  = "1.0.2"
+  base_dir = "../../frontend/"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "cloudprofile.cloud"
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read_access" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.allow_public_read_access.json
+}
+
+data "aws_iam_policy_document" "allow_public_read_access" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_object" "static_files" {
+  for_each = module.template_files.files
+
+  bucket       = aws_s3_bucket.bucket.bucket
+  key          = each.key
+  content_type = each.value.content_type
+
+  # The template_files module guarantees that only one of these two attributes
+  # will be set for each file, depending on whether it is an in-memory template
+  # rendering result or a static file on disk.
+  source  = each.value.source_path
+  content = each.value.content
+
+  # Unless the bucket has encryption enabled, the ETag of each object is an
+  # MD5 hash of that object.
+  etag = each.value.digests.md5
+}
+
+resource "aws_s3_bucket_website_configuration" "s3_website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
 }
